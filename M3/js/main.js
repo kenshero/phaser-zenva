877830ad8c734e090e8a5a6a0c47deb422fbd537
kenshero <kenshero.k@gmail.com>
2017-11-03 00:58:49 +0700

    fixed placeitem

--- M3/js/main.js
+++ M3/js/main.js
@@ -16,6 +16,8 @@ var GameState = {
   },
   create: function(){
     this.background = this.game.add.sprite(0, 0, 'backyard')
+    this.background.inputEnabled = true
+    this.background.events.onInputDown.add(this.placeItem, this)
 
     this.pet = this.game.add.sprite(100, 400, 'pet')
     this.pet.anchor.setTo(0.5)
@@ -84,6 +86,16 @@ var GameState = {
       element.alpha = 1
     })
     this.selectedItem = null
+  },
+  placeItem: function(sprite, event){
+    if(this.selectedItem && !this.uiBlocked){
+      var x = event.position.x
+      var y = event.position.y
+
+      var newItem = this.game.add.sprite(x, y, this.selectedItem.key)
+      newItem.anchor.setTo(0.5)
+      newItem.customParams = this.selectedItem.customParams
+    }
   }
 }
 
