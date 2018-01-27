905809d1157813331ed20f3b8c2e5e0fcf243530
kenshero <kenshero.k@gmail.com>
2018-01-27 21:24:53 +0700

    end M10

--- M10/js/states/Game2.js
+++ M10/js/states/Game2.js
@@ -17,12 +17,14 @@ Achicken.GameState = {
 
     this.blocksCollisionGroup = this.game.physics.p2.createCollisionGroup()
     this.enemiesCollisionGroup = this.game.physics.p2.createCollisionGroup()
-    this.chickenCollisionGroup = this.game.physics.p2.createCollisionGroup()
+    this.chickensCollisionGroup = this.game.physics.p2.createCollisionGroup();
   },
   create: function() {
 
     this.sky = this.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'sky')
     this.game.world.sendToBack(this.sky)
+
+    this.chickenHUD = this.add.group()
 
     this.enemies = this.add.group()
     this.enemies.enableBody = true
@@ -37,10 +39,17 @@ Achicken.GameState = {
     this.blocks.add(this.floor)
 
     this.floor.body.setCollisionGroup(this.blocksCollisionGroup)
-    this.floor.body.collides([this.blocksCollisionGroup, this.enemiesCollisionGroup, this.chickenCollisionGroup])
+    this.floor.body.collides([this.blocksCollisionGroup, this.enemiesCollisionGroup, this.chickensCollisionGroup])
     this.floor.body.static = true
 
     this.loadLevel()
+
+    this.pole = this.add.sprite(180, 500, 'pole')
+    this.pole.anchor.setTo(0.5, 0)
+
+    this.game.input.onDown.add(this.prepareShot, this)
+
+    this.setupChicken()
   },
 
   hitEnemy: function(bodyB, shapeA, shapeB, equation) {
@@ -51,10 +60,32 @@ Achicken.GameState = {
     )
     if(velocityDiff > Achicken.GameState.KILL_DIFF) {
       this.kill()
+
+      Achicken.GameState.updateDeadCount()
     }
   },
   update: function() {
+    if(this.isPreparingShot) {
+      this.chicken.x = this.game.input.activePointer.position.x
+      this.chicken.y = this.game.input.activePointer.position.y
 
+      var distance = Phaser.Point.distance(this.chicken.position, this.pole.position)
+
+      if(distance > this.MAX_DISTANCE_SHOOT) {
+        this.isPreparingShot = false
+        this.isChickenReady = true
+
+        this.chicken.x = this.pole.x
+        this.chicken.y = this.pole.y
+      }
+
+      if(this.game.input.activePointer.isUp) {
+        console.log("Now Shoot");
+        this.isPreparingShot = false
+
+        this.throwChicken()
+      }
+    }
   },
   gameOver: function() {
     this.game.state.start('Game', true, false, this.currentLevel)
@@ -69,6 +100,10 @@ Achicken.GameState = {
     this.levelData.enemies.forEach(function(enemy) {
       this.createEnemy(enemy)
     }, this)
+
+    this.countDeadEnemies = 0
+    this.totalNumEnemies = this.levelData.enemies.length
+    this.numChickens = 3
   },
   createBlock: function(data) {
     var block = new Phaser.Sprite(this.game, data.x, data.y, data.asset)
@@ -78,7 +113,7 @@ Achicken.GameState = {
 
     block.body.setCollisionGroup(this.blocksCollisionGroup)
 
-    block.body.collides([this.blocksCollisionGroup, this.enemiesCollisionGroup, this.chickenCollisionGroup])
+    block.body.collides([this.blocksCollisionGroup, this.enemiesCollisionGroup, this.chickensCollisionGroup])
 
     return block
   },
@@ -88,10 +123,71 @@ Achicken.GameState = {
 
     enemy.body.setCollisionGroup(this.enemiesCollisionGroup)
 
-    enemy.body.collides([this.blocksCollisionGroup, this.enemiesCollisionGroup, this.chickenCollisionGroup])
+    enemy.body.collides([this.blocksCollisionGroup, this.enemiesCollisionGroup, this.chickensCollisionGroup])
     enemy.body.onBeginContact.add(this.hitEnemy, enemy)
 
     return enemy
+  },
+  prepareShot: function(event) {
+    if(this.isChickenReady) {
+      this.isPreparingShot = true
+      this.isChickenReady = false
+    }
+  },
+  setupChicken: function() {
+    this.chicken = this.add.sprite(this.pole.x, this.pole.y, 'chicken')
+    this.chicken.anchor.setTo(0.5)
+
+    this.isChickenReady = true
+
+    this.refreshStats()
+  },
+  throwChicken: function() {
+    this.game.physics.p2.enable(this.chicken)
+
+    this.chicken.body.setCollisionGroup(this.chickensCollisionGroup)
+    this.chicken.body.collides([this.blocksCollisionGroup, this.enemiesCollisionGroup, this.chickensCollisionGroup]);
+
+    var diff = Phaser.Point.subtract(this.pole.position, this.chicken.position)
+
+    console.log("diff : ", diff);
+
+    this.chicken.body.velocity.x = Math.abs(diff.x * this.SHOOT_FACTOR)/(diff.x * this.SHOOT_FACTOR) * Math.min(Math.abs(diff.x * this.SHOOT_FACTOR), this.MAX_SPEED_SHOOT)
+    this.chicken.body.velocity.y = Math.abs(diff.y * this.SHOOT_FACTOR)/(diff.y * this.SHOOT_FACTOR) * Math.min(Math.abs(diff.y * this.SHOOT_FACTOR), this.MAX_SPEED_SHOOT)
+
+    this.endTurn()
+  },
+  updateDeadCount: function() {
+    this.countDeadEnemies++
+
+    if(this.countDeadEnemies == this.totalNumEnemies) {
+      console.log("you won");
+      this.gameOver()
+    }
+  },
+  endTurn: function() {
+    this.numChickens--
+
+    this.game.time.events.add(3 * Phaser.Timer.SECOND, function(){
+      this.chicken.kill()
+
+      this.game.time.events.add(Phaser.Timer.SECOND, function(){
+        if(this.numChickens > 0) {
+          this.setupChicken()
+        } else {
+          this.gameOver()
+        }
+      }, this)
+    }, this)
+  },
+  refreshStats: function() {
+    this.chickenHUD.removeAll()
+
+    var i = 0
+    while(i < this.numChickens) {
+      this.chickenHUD.create(this.game.width - 100 - i * 80, 30, 'chicken')
+      i++
+    }
   }
 
 };
